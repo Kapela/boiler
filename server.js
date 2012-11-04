c3fb584c10dcaaa32c2ed776cbb9b1455bf7888f
Kevin Allen <info@server>
2012-11-04 15:34:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 require('coffee-script');
 
-var express = require('express');
+var express = require('express'),
+stylus = require('stylus');
+
+require('express-namespace');
 
 var app = express();
+
+// Configuration
+
+app.set('view engine', 'jade');
+
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(require('connect-assets')());
+app.use(app.router);
+
+server = app.listen(app.settings.port);
+console.log("Express server listening on port %d in %s mode", app.settings.port, app.settings.env);
