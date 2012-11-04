b4a37ba43930a503f3876eb650499417e355f931
Kevin Allen <info@server>
2012-11-04 16:17:07 +0000

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,32 @@ var app = express();
 // Configuration
 
 app.set('view engine', 'jade');
+app.set('port', 3000);
+
 
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(require('connect-assets')());
+app.use(express.static(__dirname + '/public'));
 app.use(app.router);
+
+app.configure('development', function () {
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+});
+
+app.configure('test', function (){
+  app.set('port', 3001);
+});
+
+app.configure('production', function (){
+  app.use(express.errorHandler());
+});
+
+
+
 
 server = app.listen(app.settings.port);
 console.log("Express server listening on port %d in %s mode", app.settings.port, app.settings.env);
