59cea5926b36d5d0a9416301e19ba2ce4a1cd321
Kevin Allen <kevin.allen1994@gmail.com>
2013-03-17 17:59:14 +0000

    Add compass gemfile

---
+++ config.rb
@@ -0,0 +1,24 @@
+# Compass CSS framework config file
+
+require 'susy'
+#require 'modular-scale'
+#require 'sassy-buttons'
+#require 'breakpoint'
+#require 'compass-css-arrow'
+#require 'rgbapng'
+# Require any additional plugins here.
+require 'compass_twitter_bootstrap'
+
+
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir ="assets/stylesheets"
+css_dir = "public/stylesheets"
+images_dir = "public/images"
+fonts_dir = "public/fonts"
+javascripts_dir = "public/javascripts"
+line_comments = false
+preferred_syntax = :sass
+output_style = :expanded
+relative_assets = true
